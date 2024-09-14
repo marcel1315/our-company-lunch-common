2158314fe935419ba8346acc3b0b1ce6521d184f
Marcel Lee <chungchung1315@gmail.com>
2024-09-14 17:36:57 +0900

    feat: Add conditional to auto configuration

--- src/main/java/com/marceldev/ourcompanylunchcommon/CommonAutoConfiguration.java
+++ src/main/java/com/marceldev/ourcompanylunchcommon/CommonAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.marceldev.ourcompanylunchcommon;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,13 @@ public class CommonAutoConfiguration {
   private int expiredInHour;
 
   @Bean
+  @ConditionalOnMissingBean
   public JwtAuthenticationFilter jwtAuthenticationFilter() {
     return new JwtAuthenticationFilter(tokenProvider());
   }
 
   @Bean
+  @ConditionalOnMissingBean
   public TokenProvider tokenProvider() {
     return new TokenProvider(expiredInHour, secret);
   }
