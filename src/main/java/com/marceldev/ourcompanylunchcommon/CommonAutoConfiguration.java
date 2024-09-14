cc011eab884cb36965957d0e5c4ecc577e5be732
Marcel Lee <chungchung1315@gmail.com>
2024-09-14 16:35:50 +0900

    feat: Apply AutoConfiguration

---
+++ src/main/java/com/marceldev/ourcompanylunchcommon/CommonAutoConfiguration.java
@@ -0,0 +1,27 @@
+package com.marceldev.ourcompanylunchcommon;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "common.jwt", name = {"secret", "expired-in-hour"})
+public class CommonAutoConfiguration {
+
+  @Value("${common.jwt.secret}")
+  private String secret;
+
+  @Value("${common.jwt.expired-in-hour}")
+  private int expiredInHour;
+
+  @Bean
+  public JwtAuthenticationFilter jwtAuthenticationFilter() {
+    return new JwtAuthenticationFilter(tokenProvider());
+  }
+
+  @Bean
+  public TokenProvider tokenProvider() {
+    return new TokenProvider(expiredInHour, secret);
+  }
+}
